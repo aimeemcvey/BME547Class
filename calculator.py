82aff23f7c232f081a882d539a0f0c627ee2f2e0
Aimee McVey <aimee.mcvey@duke.edu>
2020-01-17 12:56:12 -0500

    Added divide function and input ability

--- calculator.py
+++ calculator.py
@@ -1,32 +1,43 @@
 # calculator.py
+u = int(input('Insert first number: '))
+v = int(input('Insert second number: '))
 def add(x, y):
     z = x + y
-    print('{} + {} = {}' .format(x, y, z))
-    return z
+    symbol = '+'
+    return z, symbol
 
 
 def subtract(x, y):
     z = x - y
-    print('{} - {} = {}' .format(x, y, z))
     return z
 
 
 def multiply(x, y):
     z = x * y
-    print('{} * {} = {}' .format(x, y, z))
+    return z
+
+
+def divide(x, y):
+    z = x / y
     return z
 
 
 x = input('Enter a letter: ')
 print('You entered {}' .format(x))
 if x == 'a':
-    d = add(100, 2)
+    d, f = add(u, v)
+    print('{} {} {} = {}' .format(u, f, v, d))
     if d > 100:
         print('This number is too high.')
 elif x == 's':
-    d = subtract(6, 1)
+    d = subtract(u, v)
+    print('{} - {} = {}' .format(u, v, d))
 elif x == 'm':
-    d = multiply(7, 9)
+    d = multiply(u, v)
+    print('{} * {} = {}' .format(u, v, d))
+elif x == 'd':
+    d = divide(u, v)
+    print('{} / {} = {}' .format(u, v, d))
 else:
     print('{} not recognized' .format(x))
 print("Finished")
